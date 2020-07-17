0577465c867f8857441772651dd6c70c2b901f12
saianuragt <saianuragt@gmail.com>
2020-07-17 09:59:48 +0530

    FSD0620B105

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,21 @@
+package controller;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception 	{
+		SkillDAO skilldao=new SkillDAO();
+		ArrayList<Skill>  list= (ArrayList<Skill>) skilldao.listAllSkills();
+		System.out.println("List of all Skills");
+		for(Skill s: list)
+		{
+			System.out.println(s.getSkillId()+"    "+s.getSkillName());
+		}
+	}
+}
